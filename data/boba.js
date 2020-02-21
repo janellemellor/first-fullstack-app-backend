e8a89cdb875ca7d158916480e4893963f11b8d94
janelle mellor <janellemellor@gmail.com>
2020-02-21 11:36:00 -0800

    continue setting up files

---
+++ data/boba.js
@@ -0,0 +1,50 @@
+module.exports = [
+    {
+        flavor: 'black milk tea',
+        itemNumber: 1,
+        type: 'tapioca',
+        isMilkTea: true,
+        image: './'
+    },
+
+    {
+        flavor: 'green milk tea',
+        itemNumber: 2,
+        type: 'chia',
+        isMilkTea: true,
+        image: './'
+    },
+
+    {
+        flavor: 'lychee',
+        itemNumber: 3,
+        type: 'jellies',
+        isMilkTea: false,
+        image: './'
+    },
+
+    {
+        flavor: 'taro',
+        itemNumber: 4,
+        type: 'tapioca',
+        isMilkTea: true,
+        image: './'
+    },
+
+    {
+        flavor: 'mango',
+        itemNumber: 5,
+        type: 'jellies',
+        isMilkTea: false,
+        image: './'
+    },
+
+    {
+        flavor: 'thai tea',
+        itemNumber: 6,
+        type: 'tapioca',
+        isMilkTea: true,
+        image: './'
+    },
+
+];
