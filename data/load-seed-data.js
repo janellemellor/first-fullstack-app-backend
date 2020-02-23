b4bc3b871035cf0a9f877c20e3e88578085b45e3
janelle mellor <janellemellor@gmail.com>
2020-02-23 09:35:04 -0800

    load data

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -16,10 +16,14 @@ async function run() {
         await Promise.all(
             // map every item in the array data
             data.map(item => {
-
                 // Use a "parameterized query" to insert the data,
+                return client.query(`
+                    INSERT INTO bobas (flavor, type, is_milk_tea, url, star_rating)
+                    VALUES ($1, $2, $3, $4, $5);
+                `,
+                [item.flavor, item.type, item.is_milk_tea, item.url, item.star_rating]
+                );
                 // Don't forget to "return" the client.query promise!
-                
             })
         );
 
